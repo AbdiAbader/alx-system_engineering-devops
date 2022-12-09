99198a588f61ae83621308cf0847a685170dc105
AbdiAbader <abdiprocode@gmail.com>
2022-12-09 18:57:20 +0300

    update file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,10 @@
+# Using puppet, create a file with below specifications
+
 file { '/tmp/school':
   ensure  => file,
   path    => '/tmp/school',
+  mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  mode    => '0744',
   content => 'I love Puppet'
 }
