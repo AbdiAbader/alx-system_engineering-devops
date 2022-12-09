ed9f78a13234d5371756ba2542e1b159561a41ca
AbdiAbader <abdiprocode@gmail.com>
2022-12-09 18:52:42 +0300

    first commit

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
 file { '/tmp/school':
-  ensure => file,
-  owner  => "www-data",
-  group  => "www-data",
-  mode   => "0744",
+  ensure  => file,
+  path    => '/tmp/school',
+  owner   => "www-data",
+  group   => "www-data",
+  mode    => "0744",
   content => "I love Puppet"
 }
