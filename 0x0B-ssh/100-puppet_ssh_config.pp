8e850f70ac195ab6919f1624fbbf476efb8a238b
AbdiAbader <abdiprocode@gmail.com>
2022-12-11 23:10:31 +0300

    update advance task

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,9 +2,5 @@
 file_line { 'Declare_identity_file':
   path    => '/etc/ssh/ssh_config',
   line    => 'IdentityFile ~/.ssh/school',
-}
-
-file_line { 'Turn_off_passwd_auth':
-  path    => '/etc/ssh/ssh_config',
   line    => 'PasswordAuthentication no',
 }
