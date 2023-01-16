62768864e764bf9ac0c69362d967689806540c98
AbdiAbader <abdiprocode@gmail.com>
2023-01-16 15:26:40 +0300

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,27 +1,34 @@
 #!/usr/bin/python3
-""" Api """
-
-
+"""
+Request from API; Return TODO list progress given employee ID
+"""
 import requests
-import sys
+from sys import argv
+
+
+def display():
+    """return API data"""
+    users = requests.get("http://jsonplaceholder.typicode.com/users")
+    for u in users.json():
+        if u.get('id') == int(argv[1]):
+            EMPLOYEE_NAME = (u.get('name'))
+            break
+    TOTAL_NUM_OF_TASKS = 0
+    NUMBER_OF_DONE_TASKS = 0
+    TASK_TITLE = []
+    todos = requests.get("http://jsonplaceholder.typicode.com/todos")
+    for t in todos.json():
+        if t.get('userId') == int(argv[1]):
+            TOTAL_NUM_OF_TASKS += 1
+            if t.get('completed') is True:
+                    NUMBER_OF_DONE_TASKS += 1
+                    TASK_TITLE.append(t.get('title'))
+    print("Employee {} is done with tasks({}/{}):".format(EMPLOYEE_NAME,
+                                                          NUMBER_OF_DONE_TASKS,
+                                                          TOTAL_NUM_OF_TASKS))
+    for task in TASK_TITLE:
+        print("\t {}".format(task))
 
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/users/"
-    user_id = sys.argv[1]
-    user = requests.get(url + user_id).json()
-    todo = requests.get(url + user_id + "/todos").json()
-    name = user.get("name")
-    username = user.get("username")
-    tasks = []
-    for task in todo:
-        tasks.append(task.get("title"))
-        completed = task.get("completed")
-        with open("{}.csv".format(user_id), "a") as f:
-            f.write('"{}","{}","{}","{}"'.format(user_id, username, completed,
-                                                 name))
-            f.write("")
-    print("Employee {} is done with tasks({}/{}):".format(name, len(tasks),
-                                                          len(todo)))
-    for task in tasks:
-        print("\t {}".format(task))
+    display()
