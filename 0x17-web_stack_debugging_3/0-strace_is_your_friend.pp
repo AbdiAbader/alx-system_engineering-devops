34a20c93b4774a55c82aad8ad66141e5b182f6a0
AbdiAbader <abdiprocode@gmail.com>
2023-02-01 14:10:14 +0300

    first commit

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,6 @@
 # using Strace to debug a program
 
-exec { 'strace' } { /usr/bin/strace -o /tmp/strace.log -f -tt -T -s 1024 -p $pid }
+exec { 'strace' :
+      path => '/usr/bin/strace' , 
+      args => [ '-o' , '/tmp/strace.log' , '-f' , '/usr/bin/ls' , '-l' ] 
+      }
