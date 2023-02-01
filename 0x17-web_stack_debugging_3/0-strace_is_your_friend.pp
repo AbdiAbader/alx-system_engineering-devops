f664d95755f0f25f471ee61e9df93adad1db3619
AbdiAbader <abdiprocode@gmail.com>
2023-02-01 14:55:04 +0300

    fix error

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
 # using Strace to debug a program
 
-exec {'fix-wordpress-permissions':
-  command => "sed -i 's/.phpp/.php/' /var/www/html/wp-config.php",
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+exec {'fix-wordpress':
+  command => "sed -i 's/.phpp/.php/' /var/www/html/wp-config.php"
 }
