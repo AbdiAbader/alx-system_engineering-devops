efc8caf549bf102780fe477b5c327a3926a253fd
AbdiAbader <abdiprocode@gmail.com>
2023-02-09 14:41:24 +0300

    task 1

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,12 @@
+# user limit
+
+exec {"user limit":
+    command => 'sed -i "/holberton hard nofile 1024/d" /etc/security/limits.conf',
+    path   => '/bin:/usr/bin',
+}
+
+exec {"increase":
+    command => 'sed -i "/holberton soft nofile 1024/a" /etc/security/limits.conf',
+    path   => '/bin:/usr/bin',
+
+}
