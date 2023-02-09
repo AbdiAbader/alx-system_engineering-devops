0f9ac0969b684533651510139c498518f1e6ee14
AbdiAbader <abdiprocode@gmail.com>
2023-02-09 14:44:05 +0300

    fix indetattion error

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,12 +1,12 @@
 # user limit
 
 exec {"user limit":
-    command => 'sed -i "/holberton hard nofile 1024/d" /etc/security/limits.conf',
-    path   => '/bin:/usr/bin',
+  command => 'sed -i "/holberton hard nofile 1024/d" /etc/security/limits.conf',
+  path    => '/bin:/usr/bin:/sbin:/usr/sbin',
 }
 
 exec {"increase":
-    command => 'sed -i "/holberton soft nofile 1024/a" /etc/security/limits.conf',
-    path   => '/bin:/usr/bin',
+  command => 'sed -i "/holberton soft nofile 1024/a" /etc/security/limits.conf',
+  path    => '/bin:/usr/bin',
 
 }
