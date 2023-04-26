87ebea0a4cfb4366eeb6d5341c83acc0fe9ecb77
AbdiAbader <105592398+AbdiAbader@users.noreply.github.com>
2023-04-26 15:04:55 +0300

    Update 1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,13 +1,9 @@
-# user limit
-
-exec {'user limit':
-  command  => 'sudo sed -i \'s/nofile 4/nofile 10000/\' /etc/security/limits.conf',
+# limit user
+exec { 'hard corrections':
+  command  => 'sudo sed -i \'s/nofile 5/nofile 30000/\' /etc/security/limits.conf',
   provider => shell,
-
 }
-
-exec {'user soft':
+exec { 'only soft':
   command  => 'sudo sed -i \'s/nofile 4/nofile 10000/\' /etc/security/limits.conf',
   provider => shell,
-
 }
